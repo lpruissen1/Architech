c65ac610f80f1848dee7f0fd4ea8a74556548b0d
Alexander Davis <alexander96davis@gmail.com>
2021-04-14 21:05:28 -0400

    delete errythang

--- src/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "Backend/API"
-
-func main() {
-	api := API.NewApiService()
-	api.Initialize()
-	api.Run()
-}
--- src/web/services/api/CustomIndices.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export default
-{
-  GetIndex(id) {
-    return axios.get(`http://localhost:8081/api/v1/index/${id}`)
-      .then((response) => response.data);
-  },
-};
