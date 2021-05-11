21d95fa148de8ffef09bdc952e635c9703f6446b
lpruissen1 <46303264+lpruissen1@users.noreply.github.com>
2021-05-10 21:10:19 -0400

    show add rule button again after rule selection

--- Web/Web/ClientApp/src/components/Screener/Subcomponents/RuleSelector.js
+++ Web/Web/ClientApp/src/components/Screener/Subcomponents/RuleSelector.js
@@ -5,11 +5,16 @@ export default function RuleSelector(props) {
 	const [displayList, setDisplayList] = useState(false);
 	const clickie = () => setDisplayList(!displayList)
 
+	const superClickie = (event) => {
+		setDisplayList(!displayList)
+		props.handleAddNewMetricClick(event)
+	}
+
 	const renderOptions = () => {
 		return (
 			<>
 				{props.options && props.options.map((option) =>
-					<button key={ option.value} className="newMetricButton" type={option.type} value={option.value} onClick={props.handleAddNewMetricClick}> {option.displayName} </button>
+					<button key={option.value} className="newMetricButton" type={option.type} value={option.value} onClick={superClickie}> {option.displayName} </button>
 				)}
 			</>
 		)
