5e86344c5bfdaa16a858e07fdaee635ea3a1050f
lpruissen1 <46303264+lpruissen1@users.noreply.github.com>
2021-05-10 22:46:31 -0400

    Add timedRange

--- Web/Web/ClientApp/src/components/Screener/Subcomponents/RangeSelector.js
+++ Web/Web/ClientApp/src/components/Screener/Subcomponents/RangeSelector.js
@@ -13,3 +13,16 @@ export default function RangeSelector(props) {
 		</>
 	);
 }
+
+export default function TimedRangeSelector(props) {
+
+	// pass in initial input min/max and min/max for entire slider
+	// we need an id for each rule so we can keep a list for display and removal purposes
+	// add timespan dropdown (should be own component)
+	return (
+		<>
+			<div>{props.displayName}</div>
+			<Range min={0} max={20} defaultValue={[3, 10]} />
+		</>
+	);
+}
