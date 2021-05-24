9046b0e7bc704bf3bbf600bef23e67df1722dc51
Alexander Davis <alexander96davis@gmail.com>
2021-05-23 23:35:39 -0400

    Make Users API request

--- Web/Web/ClientApp/src/components/Login/Registration.js
+++ Web/Web/ClientApp/src/components/Login/Registration.js
@@ -1,5 +1,6 @@
-﻿import React from 'react';
+﻿import React, { useState } from 'react'
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 import './Registration.css';
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -15,11 +16,45 @@ const useStyles = makeStyles((theme) => ({
 		margin: theme.spacing(1.5),
 		width: '43ch'
 	}
-	}));
+}));
+
 
 export function Registration() {
 
 	const classes = useStyles();
+	const [firstName, setFirstName] = useState('');
+	const [lastName, setLastName] = useState('');
+	const [username, setUsername] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+	const postUserRegistrationRequest = (data) => {
+		fetch("https://localhost:44327/User", {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(data)
+		})
+			.then(function (response) {
+				return response.status
+			});
+	}
+
+
+	const registerUser = () => {
+		return postUserRegistrationRequest({
+			"firstName": firstName,
+			"lastName": lastName,
+			"userName": username,
+			"email": email,
+			"password": password
+		});
+	}
+
+	const register = () => {
+		registerUser()
+	}
 
 	return (
 		<div className="registration-card">
@@ -29,27 +64,48 @@ export function Registration() {
 					<div className="nameRow">
 						<TextField required id="outlined-required" className={classes.smallForm} label="Required" variant="outlined" placeholder="First Name"
 							InputLabelProps={{
-							shrink: true,
-						}}/>
+								shrink: true,
+							}}
+							onChange={(event) => {
+								setFirstName(event.target.value)
+							}}
+						/>
 						<TextField required id="outlined-required" className={classes.smallForm} label="Required" variant="outlined" placeholder="Last Name"
 							InputLabelProps={{
 							shrink: true,
-						}} />
+							}}
+							onChange={(event) => {
+								setLastName(event.target.value)
+							}}
+						/>
 					</div>
 					<TextField required id="outlined-required" className={classes.largeForm} label="Required" variant="outlined" placeholder="Username"
 						InputLabelProps={{
 						shrink: true,
-					}}/>
+						}}
+						onChange={(event) => {
+							setUsername(event.target.value)
+						}}
+					/>
 					<TextField required id="outlined-required" className={classes.largeForm} label="Required" variant="outlined" placeholder="Password"
 						InputLabelProps={{
 						shrink: true,
-					}}/>
+						}}
+						onChange={(event) => {
+							setPassword(event.target.value)
+						}}
+					/>
 					<TextField required id="outlined-required" className={classes.largeForm} label="Required" variant="outlined" placeholder="Email"
 						InputLabelProps={{
 						shrink: true,
-					}}/>
+						}}
+						onChange={(event) => {
+							setEmail(event.target.value)
+						}}
+					/>
 				</div>
 			</form>
+			<Button onClick={register}> Register </Button>
 		</div>
 	);
 }
