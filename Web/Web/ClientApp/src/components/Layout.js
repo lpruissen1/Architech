4e99f69dae166adbe8f290c5838e66e280672a9b
Alexander Davis <alexander96davis@gmail.com>
2021-06-02 08:01:47 -0400

    Get request for custom indices

--- Web/Web/ClientApp/src/components/Layout.js
+++ Web/Web/ClientApp/src/components/Layout.js
@@ -46,7 +46,7 @@ export class Layout extends Component {
 				<Container>
 					<Route exact path='/' component={Home} />
 					<AuthenticatedRoute exact path='/screener' component={() => <Screener userID={this.state.userID} />} loggedIn={this.state.loggedIn} />
-					<AuthenticatedRoute exact path='/portfolios' component={Portfolios} loggedIn={this.state.loggedIn} userID={this.state.userID} />
+					<AuthenticatedRoute exact path='/portfolios' component={() => <Portfolios userID={this.state.userID} />} loggedIn={this.state.loggedIn} />
 					<Route exact path='/login' component={() => <Login updateLoggedIn={this.updateLoggedIn} setUserID={this.setUserID} />} />
 					<Route exact path='/register' component={() => <Registration updateLoggedIn={this.updateLoggedIn} setUserID={this.setUserID} />} />
 					<AuthenticatedRoute exact path='/research' component={Research} loggedIn={this.state.loggedIn} />
--- Web/Web/ClientApp/src/components/Portfolios/Portfolios.js
+++ Web/Web/ClientApp/src/components/Portfolios/Portfolios.js
@@ -4,15 +4,30 @@ import Card from 'react-bootstrap/Card';
 import { useHistory } from 'react-router-dom';
 import './Portfolios.css';
 
-export function Portfolios() {
+export function Portfolios(props) {
 
 	const history = useHistory();
 	const handleOnClick = useCallback(() => history.push('/screener'), [history]);
 	const [createNew, setCreateNew] = useState(false)
 	const clickie = () => setCreateNew(!createNew)	
+	const API_URL = 'https://localhost:7001/CustomIndex?userID=' + props.userID
+
+	const getCustomIndexRequest = () => {
+		debugger
+		fetch(API_URL, {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+			.then(function (response) {
+				return response.status
+			});
+	}
 
 	return (
 		<div>
+			<button onClick={getCustomIndexRequest}> make request </button>
 			<h1>My Blueprints</h1>
 			<Card className='portfoliosCard'>
 					<NewPortfolioCard onClick={clickie} />
