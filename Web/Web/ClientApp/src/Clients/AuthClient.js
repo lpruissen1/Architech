5601931967d0513a8ba766cba0fad607622485cf
lpruissen1 <46303264+lpruissen1@users.noreply.github.com>
2021-06-10 19:03:35 -0400

    Remove hash

--- Web/Web/ClientApp/src/Clients/AuthClient.js
+++ Web/Web/ClientApp/src/Clients/AuthClient.js
@@ -11,7 +11,7 @@ class AuthService {
 			headers: {
 				'Content-Type': 'application/json'
 			},
-			body: JSON.stringify({ "username": username, "passwordhash": password })
+			body: JSON.stringify({ "username": username, "password": password })
 		});
 
 		if (response.ok) {
