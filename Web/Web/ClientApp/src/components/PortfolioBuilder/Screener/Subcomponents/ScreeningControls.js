aa5c6be0001ad01319f43fa14b0cc89fb6366e37
Alexander Davis <alexander96davis@gmail.com>
2021-07-04 22:40:17 -0400

    More portfolio builder styling

--- Web/Web/ClientApp/src/components/PortfolioBuilder/Screener/Subcomponents/ScreeningControls.js
+++ Web/Web/ClientApp/src/components/PortfolioBuilder/Screener/Subcomponents/ScreeningControls.js
@@ -4,24 +4,29 @@ import InclusionsExclusions from './InclusionsExclusions';
 import MarketSelector from './MarketSelector';
 import BasicRulesSection from "./Rules/BasicRulesSection";
 import SectorSelector from './SectorSelector';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
 export default function ScreeningControls(props) {
 
 	return (
 		<Grid container spacing={1}>
 			<Grid item xs={12}>
+				<Typography style={{ marginTop: 20, marginLeft: 10 }} variant='h6'>Choose Your Markets</Typography>
 				<MarketSelector
 					markets={props.markets}
 					handleUpdate={props.handleUpdate}
-				/>
+					/>
 			</Grid>
 			<Grid item xs={12}>
+				<Typography style={{ marginLeft: 10 }} variant='h6'>Choose Your Sectors</Typography>
 				<SectorSelector
 					sectors={props.sectors}
 					handleUpdate={props.handleUpdate}
-				/>
+					/>
 			</Grid>
 			<Grid item xs={12}>
+				<Box className="rule-selector-container">
 				<BasicRulesSection
 					rangedRules={props.rangedRules}
 					timedRangeRules={props.timedRangeRules}
@@ -32,18 +37,21 @@ export default function ScreeningControls(props) {
 					deleteTimedRangeRule={props.deleteTimedRangeRule}
 					checkIfRangedRuleExists={props.checkIfRangedRuleExists}
 					checkIfTimedRangeRuleExists={props.checkIfTimedRangeRuleExists}
-				/>
+					/>
+				</Box>
 			</Grid>
 			<Grid item xs={12}>
-				<InclusionsExclusions
-					inclusions={props.inclusions}
-					exclusions={props.exclusions}
-					handleUpdate={props.handleUpdate}
-					AddInclusion={props.AddInclusion}
-					AddExclusion={props.AddExclusion}
-					DeleteInclusion={props.DeleteInclusion}
-					DeleteExclusion={props.DeleteExclusion}
-				/>
+				<Box className="inclusion-selector-container">
+					<InclusionsExclusions
+						inclusions={props.inclusions}
+						exclusions={props.exclusions}
+						handleUpdate={props.handleUpdate}
+						AddInclusion={props.AddInclusion}
+						AddExclusion={props.AddExclusion}
+						DeleteInclusion={props.DeleteInclusion}
+						DeleteExclusion={props.DeleteExclusion}
+					/>
+				</Box>
 			</Grid>
 		</Grid>
 	)
