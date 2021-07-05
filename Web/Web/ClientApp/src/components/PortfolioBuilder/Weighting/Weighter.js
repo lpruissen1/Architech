f530baa08dccabddf902a77a375ef9faa9bcb5b5
lpruissen1 <46303264+lpruissen1@users.noreply.github.com>
2021-07-05 12:22:39 -0400

    Add buttons

--- Web/Web/ClientApp/src/components/PortfolioBuilder/Weighting/Weighter.js
+++ Web/Web/ClientApp/src/components/PortfolioBuilder/Weighting/Weighter.js
@@ -1,6 +1,51 @@
-﻿import React, { useState } from 'react'
+﻿import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import React, { useState } from 'react';
+
+const useStyles = makeStyles((theme) => ({
+	button: {
+		margin: theme.spacing(2),
+		textTransform: 'none',
+		fontSize: 14,
+		fontWeight: 500,
+		minWidth: 200
+	}
+}));
 
 export function Weighter(props) {
-	const [weights, setWeights] = useState([])
-	return (<>Fuck</>);
+	const classes = useStyles();
+	const [options, _] = useState(GetWeightingOptions())
+	const [selection, setSelection] = useState("")
+
+	const handleCheck = (event) => {
+		setSelection(event.currentTarget.value)
+	}
+
+	return (
+		<Grid container spacing={1}>
+			{options.map(option => {
+				return (
+					<Grid item xs={12/options.length} key={option.value}>
+						<Button
+							onClick={handleCheck}
+							value={option.value}
+							style={{ outline: 'none' }}
+							className={classes.button}
+							variant={option.value === selection ? 'contained' : 'outlined'}
+							color={option.value === selection ? 'primary' : 'default'}
+							disableElevation
+							disableRipple
+						>
+							{option.displayName}
+						</Button>
+					</Grid>
+				)
+			})}
+		</Grid>)
+}
+
+
+const GetWeightingOptions = () => {
+	return ([{ displayName: "Equally", value: "Equal" }, { displayName: "By Market Capitalization", value: "MarketCap" }, { displayName: "By Dividend Yield", value: "DividendYield"}]);
 }
