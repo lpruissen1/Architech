51ec497acccf91b1d78f7e1259f0aaed385da4c4
Alexander Davis <alexander96davis@gmail.com>
2021-07-24 21:32:56 -0400

    Display profile info on page

--- Web/Web/ClientApp/src/components/Profile/Profile.js
+++ Web/Web/ClientApp/src/components/Profile/Profile.js
@@ -1,5 +1,9 @@
-﻿import React, { useEffect, useState } from 'react'
+﻿import React, { useEffect, useState } from 'react';
+import RaisedCard from '../Generic/RaisedCard';
 import UserClient from '../../Clients/UserClient';
+import Skeleton from '@material-ui/lab/Skeleton';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 
 export function Profile() {
 	const [userInfo, setUserInfo ] = useState()
@@ -7,6 +11,35 @@ export function Profile() {
 	useEffect( async () => setUserInfo(await UserClient.GetInfo()), [])
 
 	return (
-		<h1> {userInfo && userInfo.firstName} </h1>
+		<RaisedCard style={{ color: '#fff', height: 640, paddingLeft: 50, paddingRight: 50, paddingTop: 20 }}>
+				<Grid container spacing={1}>
+					<>
+						<Grid item xs={12} align="center">
+							<Grid container justifyContent='center'><Typography variant='h3'>Your Profile</Typography></Grid>
+						</Grid>
+						<Grid item xs={12} align="flex-start">
+							<Typography variant="h6"> 
+								{userInfo ? 'Personal Information' : <Skeleton />}
+							</Typography>
+							<div style={{height: 2, backgroundColor: '#d0d0d0'}}></div>
+						</Grid>
+						<Grid item xs={12} style={{ paddingTop: 10, paddingLeft: 20 }}>
+							<Typography variant="body1">
+								{userInfo ? 'Username:   ' + userInfo.username : <Skeleton />}
+							</Typography>
+						</Grid>
+						<Grid item xs={12} style={{ paddingTop: 10, paddingLeft: 20 }}>
+							<Typography variant="body1">
+								{userInfo ? 'Name:   ' + userInfo.firstName + ' ' + userInfo.lastName : <Skeleton />}
+							</Typography>
+						</Grid>
+						<Grid item xs={12} style={{ paddingTop: 10, paddingLeft: 20 }}>
+							<Typography variant="body1">
+								{userInfo ? 'Email:   ' + userInfo.email : <Skeleton />}
+							</Typography>
+						</Grid>
+					</>
+					</Grid>
+		</RaisedCard>
 	)
 }
