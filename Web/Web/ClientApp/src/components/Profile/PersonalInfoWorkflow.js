9342c19987a068f70f441b2e574067fe11fc0c18
Alexander Davis <alexander96davis@gmail.com>
2021-07-27 10:38:09 -0400

    Finish adding fields to personal info workflow

--- Web/Web/ClientApp/src/components/Profile/PersonalInfoWorkflow.js
+++ Web/Web/ClientApp/src/components/Profile/PersonalInfoWorkflow.js
@@ -8,7 +8,7 @@ export default function PersonalInfoWorkflow(props) {
 
 	return (
 		<Grid container spacing={1} style={{ paddingLeft: '5%', paddingRight: '5%' }}>
-			<Grid item xs={6} style={{ borderLeft: '1px solid #545454' }}>
+			<Grid item xs={6} style={{ borderLeft: '2px solid #545454', paddingRight: 30 }}>
 				<Grid container spacing={1}>
 					<Grid item xs={6}>
 						<OutlinedTextInput label='First Name' onChange={() => setFirstName('')} />
@@ -16,10 +16,35 @@ export default function PersonalInfoWorkflow(props) {
 					<Grid item xs={6}>
 						<OutlinedTextInput label='Last Name' onChange={() => setFirstName('')} />
 					</Grid>
+					<Grid item xs={12} style={{ paddingRight: 20 }}>
+						<OutlinedTextInput label='Street Address' width='100%' />
+					</Grid>
+					<Grid item xs={5}>
+						<OutlinedTextInput label='City' />
+					</Grid>
+					<Grid item xs={3}>
+						<OutlinedTextInput label='State' />
+					</Grid>
+					<Grid item xs={4}>
+						<OutlinedTextInput label='Postal Code' />
+					</Grid>
 				</Grid>
 			</Grid>
-			<Grid item xs={6} style={{ borderLeft: '1px solid #545454' }}>
-				<OutlinedTextInput label='First Name' style={{ fontSize: 12 }} onChange={() => setFirstName('')} />
+			<Grid item xs={6} style={{ borderLeft: '2px solid #545454', paddingRight: 30 }}>
+				<Grid container spacing={1}>
+					<Grid item xs={12} style={{ paddingRight: 20 }}>
+						<OutlinedTextInput label='Email Address' width='100%' />
+					</Grid>
+					<Grid item xs={12} style={{ paddingRight: 20 }}>
+						<OutlinedTextInput label='Phone Number' width='100%' />
+					</Grid>
+					<Grid item xs={6}>
+						<OutlinedTextInput label='Date of Birth' />
+					</Grid>
+					<Grid item xs={6}>
+						<OutlinedTextInput label='Country of Tax Residency' />
+					</Grid>
+				</Grid>
 			</Grid> 
 		</Grid>
 	)
