58cd0844c68deb360bc7958a67a902685d098415
Alexander Davis <alexander96davis@gmail.com>
2021-07-30 21:40:46 -0400

    Add formatters

--- Web/Web/ClientApp/src/components/PortfolioBuilder/Screener/Subcomponents/TickerInfoTable.js
+++ Web/Web/ClientApp/src/components/PortfolioBuilder/Screener/Subcomponents/TickerInfoTable.js
@@ -3,6 +3,7 @@ import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
+import NumberFormatter from '../../../../Formatter/NumberFormatter.js'
 
 export default function TickerInfoTable(props) {
 	const fontColor = '#d0d0d0'
@@ -12,20 +13,20 @@ export default function TickerInfoTable(props) {
 			<Table size="small">
 			<TableBody>
 				<TableRow>
-						<TableCell align='left' style={{ color: fontColor, borderBottom: 'none', fontSize: 11 }}>Name</TableCell>
-						<TableCell align='left' style={{ color: fontColor, borderBottom: 'none', fontSize: 11 }}>{props.tickerInfo[0].name}</TableCell>
+						<TableCell align='left' style={{ color: fontColor, borderBottom: 'none', fontSize: 11, borderBottom: '1px solid #545454' }}>Name</TableCell>
+						<TableCell align='left' style={{ color: fontColor, borderBottom: 'none', fontSize: 11, borderBottom: '1px solid #545454' }}>{props.tickerInfo[0].name}</TableCell>
 				</TableRow>
 				<TableRow>
-						<TableCell align='left' style={{ color: fontColor, borderBottom: 'none', fontSize: 11 }}>Sector</TableCell>
-						<TableCell align='left' style={{ color: fontColor, borderBottom: 'none', fontSize: 11 }}>{props.tickerInfo[0].sector}</TableCell>
+						<TableCell align='left' style={{ color: fontColor, borderBottom: 'none', fontSize: 11, borderBottom: '1px solid #545454' }}>Sector</TableCell>
+						<TableCell align='left' style={{ color: fontColor, borderBottom: 'none', fontSize: 11, borderBottom: '1px solid #545454' }}>{props.tickerInfo[0].sector}</TableCell>
 				</TableRow>
 				<TableRow>
-						<TableCell align='left' style={{ color: fontColor, borderBottom: 'none', fontSize: 11 }}>Market Cap</TableCell>
-						<TableCell align='left' style={{ color: fontColor, borderBottom: 'none', fontSize: 11 }}>{props.tickerInfo[0].marketCap}</TableCell>
+						<TableCell align='left' style={{ color: fontColor, borderBottom: 'none', fontSize: 11, borderBottom: '1px solid #545454' }}>Market Cap</TableCell>
+						<TableCell align='left' style={{ color: fontColor, borderBottom: 'none', fontSize: 11, borderBottom: '1px solid #545454' }}>{NumberFormatter(props.tickerInfo[0].marketCap, "MarketCap")}</TableCell>
 				</TableRow>
 				<TableRow>
-						<TableCell align='left' style={{ color: fontColor, borderBottom: 'none', fontSize: 11 }}>Current Price</TableCell>
-						<TableCell align='left' style={{ color: fontColor, borderBottom: 'none', fontSize: 11 }}>{props.tickerInfo[0].currentPrice}</TableCell>
+						<TableCell align='left' style={{ color: fontColor, borderBottom: 'none', fontSize: 11, borderBottom: '1px solid #545454' }}>Current Price</TableCell>
+						<TableCell align='left' style={{ color: fontColor, borderBottom: 'none', fontSize: 11, borderBottom: '1px solid #545454' }}>${props.tickerInfo[0].currentPrice}</TableCell>
 				</TableRow>
 			</TableBody>
 			</Table>
