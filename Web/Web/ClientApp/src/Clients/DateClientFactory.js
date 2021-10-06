045671a68e1b8ba167fea0d55996a804afc3b89f
lpruissen1 <46303264+lpruissen1@users.noreply.github.com>
2021-10-06 19:33:00 -0400

    Add real time data reading to front end

---
+++ Web/Web/ClientApp/src/Clients/DateClientFactory.js
@@ -0,0 +1,13 @@
+﻿const API_URL = "https://localhost:42069/Date/"; // this will eventually need to be config
+
+class DateClientFactory {
+	async GetDateStream() {
+		var eventSource = new EventSource(API_URL);
+		eventSource.onmessage = e => {
+			() => { return e } };
+
+		return eventSource
+	}
+}
+
+export default new DateClientFactory();
--- Web/Web/ClientApp/src/components/AppBar.js
+++ Web/Web/ClientApp/src/components/AppBar.js
@@ -1,13 +1,27 @@
 ﻿import { Box } from '@material-ui/core';
 import AppBar from '@material-ui/core/AppBar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import NavLink from 'react-router-dom/NavLink';
 import Logo from './ArchitechLogo.svg';
 import LogoFont from './ArchitechLogoFont.svg';
 import PrimaryLinkButton from './Generic/PrimaryLinkButton';
+import Typography from '@material-ui/core/Typography';
+
 import './NavMenu.css';
 
 export default function PrimarySearchAppBar() {
+	const [date, setDate] = useState("Hi")
+
+	useEffect(() => {
+		let eventSource = new EventSource("https://localhost:5001/Date")
+		eventSource.onmessage = e => updateDate(e.data)
+	}, [])
+
+	const updateDate = (newDate) => {
+		debugger
+		setDate(newDate)
+	}
+
 	return (
 		<div>
 			<AppBar position="fixed" style={{ zIndex: 10000 }}>
@@ -23,6 +37,9 @@ export default function PrimarySearchAppBar() {
 									height: 32, marginLeft: 0
 								}} src={LogoFont} alt="Architech Logo Font" />
 							</NavLink>
+						</div>
+						<div style={{ display: 'flex', width: '75%', alignItems: 'center', justifyContent: 'right' }}>
+							<Typography variant='h3' style={{ color: '#f0f0f0' }}>{date}</Typography>
 						</div>
 						<div style={{ display: 'flex', width: '75%', alignItems: 'center', justifyContent: 'right' }}>
 							<PrimaryLinkButton to="/login" text="Login" />
