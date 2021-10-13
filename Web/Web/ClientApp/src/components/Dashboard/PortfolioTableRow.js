5c7bffa0e2586864b70f0980588d0576537559f5
Alexander Davis <alexander96davis@gmail.com>
2021-10-13 12:37:42 -0400

    Holdings table styling/layout

--- Web/Web/ClientApp/src/components/Dashboard/PortfolioTableRow.js
+++ Web/Web/ClientApp/src/components/Dashboard/PortfolioTableRow.js
@@ -33,21 +33,21 @@ export default function PortfolioTableRow(props) {
 	return (
 		<>
 			<TableRow>
-				<TableCell style={{ color: '#d0d0d0', borderTop: '1px solid #606060', borderBottom: 'none', width: 40 }}>
+				<TableCell style={{ color: '#f0f0f0', borderTop: '1px solid #606060', borderBottom: 'none', width: 40 }}>
 					<IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)} style={{ outline: 'none', color: '#c0c0c0' }}>
 						{open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
 					</IconButton>
 				</TableCell>
-				<TableCell style={{ color: '#d0d0d0', borderTop: '1px solid #606060', borderBottom: 'none' }}>
+				<TableCell style={{ color: '#f0f0f0', borderTop: '1px solid #606060', borderBottom: 'none' }}>
 					{props.portfolio.portfolioId}
 				</TableCell>
-				<TableCell style={{ color: '#d0d0d0', borderTop: '1px solid #606060', borderBottom: 'none' }}>
+				<TableCell style={{ color: '#f0f0f0', borderTop: '1px solid #606060', borderBottom: 'none' }}>
 					$2,395.42
 				</TableCell>
-				<TableCell style={{ color: '#d0d0d0', borderTop: '1px solid #606060', borderBottom: 'none' }}>
+				<TableCell style={{ color: '#f0f0f0', borderTop: '1px solid #606060', borderBottom: 'none' }}>
 					$415.26 (21.21%)
 				</TableCell>
-				<TableCell style={{ color: '#d0d0d0', borderTop: '1px solid #606060', borderBottom: 'none', alignItems: 'center' }}>
+				<TableCell style={{ color: '#f0f0f0', borderTop: '1px solid #606060', borderBottom: 'none', alignItems: 'center' }}>
 					<Link to='/trade' style={{ textDecoration: 'none' }}>
 						<Button className={classes.button}>Buy/Sell</Button>
 					</Link>
@@ -57,39 +57,40 @@ export default function PortfolioTableRow(props) {
 					<TableCell style={{ paddingBottom: 0, paddingTop: 0, borderBottom: 'none', borderTop: '1px solid #606060', backgroundColor: '#484848' }} colSpan={5}>
 						<Collapse in={open} timeout="auto" unmountOnExit>
 							<Box margin={1}>
-							<Typography variant="subtitle1" gutterBottom component="div">
-								Holdings
-							</Typography>
 							<Table size="small" aria-label="holdings">
 								<TableHead>
 									<TableRow>
-										<TableCell style={{ color: '#d0d0d0' }}>Ticker</TableCell>
-										<TableCell style={{ color: '#d0d0d0' }}>Quantity</TableCell>
-										<TableCell style={{ color: '#d0d0d0' }}>Cost Basis</TableCell>
-										<TableCell style={{ color: '#d0d0d0' }}>Price</TableCell>
-										<TableCell style={{ color: '#d0d0d0' }}>Performance</TableCell>
-										<TableCell style={{ color: '#d0d0d0' }}></TableCell>
+										<TableCell align='left' style={{ color: '#f0f0f0', borderBottom: 'none', fontWeight: 600 }}>Ticker</TableCell>
+										<TableCell align='right' style={{ color: '#f0f0f0', borderBottom: 'none', fontWeight: 600 }}>Quantity</TableCell>
+										<TableCell align='right' style={{ color: '#f0f0f0', borderBottom: 'none', fontWeight: 600 }}>Cost Basis</TableCell>
+										<TableCell align='right' style={{ color: '#f0f0f0', borderBottom: 'none', fontWeight: 600 }}>Price</TableCell>
+										<TableCell align='right' style={{ color: '#f0f0f0', borderBottom: 'none', fontWeight: 600 }}>Performance</TableCell>
+										<TableCell style={{ color: '#f0f0f0', borderBottom: 'none', fontWeight: 600 }}></TableCell>
 									</TableRow>
 								</TableHead>
 								<TableBody>
 									{props.portfolio.positions.map((position) => (
 										<TableRow key={position.ticker}>
-											<TableCell component="th" scope="row" style={{ color: '#d0d0d0' }}>
+											<TableCell align='left' component="th" scope="row" style={{ color: '#c0c0c0', borderTop: '1px solid #606060', borderBottom: 'none' }}>
 												{position.ticker}
 											</TableCell>
-											<TableCell component="th" scope="row" style={{ color: '#d0d0d0' }}>
-												{position.quantity}
+											<TableCell align='right' component="th" scope="row" style={{ color: '#c0c0c0', borderTop: '1px solid #606060', borderBottom: 'none' }}>
+												{position.quantity.toFixed(5)}
 											</TableCell>
-											<TableCell component="th" scope="row" style={{ color: '#d0d0d0' }}>
-												{position.averagePurchasePrice}
+											<TableCell align='right' component="th" scope="row" style={{ color: '#c0c0c0', borderTop: '1px solid #606060', borderBottom: 'none' }}>
+												${position.averagePurchasePrice.toFixed(2)}
 											</TableCell>
-											<TableCell component="th" scope="row" style={{ color: '#d0d0d0' }}>
-												{position.currentPrice}
+											<TableCell align='right' component="th" scope="row" style={{ color: '#c0c0c0', borderTop: '1px solid #606060', borderBottom: 'none' }}>
+												${position.currentPrice.toFixed(2)}
 											</TableCell>
-											<TableCell component="th" scope="row" style={{ color: '#d0d0d0' }}>
-												{((position.currentPrice - position.averagePurchasePrice) / position.averagePurchasePrice) * 100}%
+											<TableCell align='right' component="th" scope="row" style={{ color: '#c0c0c0', borderTop: '1px solid #606060', borderBottom: 'none' }}>
+												{(((position.currentPrice - position.averagePurchasePrice) / position.averagePurchasePrice) * 100).toFixed(3)}%
 											</TableCell>
-											<TableCell style={{ color: '#d0d0d0' }}></TableCell>
+											<TableCell align='center' style={{ color: '#c0c0c0', borderTop: '1px solid #606060', borderBottom: 'none' }}>
+												<Link to='/trade' style={{ textDecoration: 'none' }}>
+													<Button className={classes.button}>Buy/Sell</Button>
+												</Link>
+											</TableCell>
 										</TableRow>
 									))}
 								</TableBody>
