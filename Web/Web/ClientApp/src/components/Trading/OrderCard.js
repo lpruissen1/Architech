ccd0fc4b3aeb424a75c2e2964ef4b2ab40a267d9
Alexander Davis <alexander96davis@gmail.com>
2021-10-18 18:39:39 -0400

    make order card

---
+++ Web/Web/ClientApp/src/components/Trading/OrderCard.js
@@ -0,0 +1,26 @@
+﻿import Typography from '@material-ui/core/Typography';
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+
+export default function OrderCard(props) {
+
+	return (
+		<div style={{ backgroundColor: '#424242', borderRadius: 4 }}>
+			<Grid container spacing={1} style={{ paddingTop: 16, paddingBottom: 16 }}>
+				<Grid item xs={3}>
+					<Typography variant='subtitle1' style={{ color: '#d0d0d0' }}>{props.order.status}</Typography>
+					<Typography style={{ fontSize: 10, color: '#848484' }}>Order Sent: {props.order.createdAt}</Typography>
+					{props.order.status === 'filled' ? <Typography style={{ fontSize: 10, color: '#848484' }}>Order Filled: {props.order.filledAt}</Typography> : <></>}
+				</Grid>
+				<Grid item xs={9}>
+					<Typography style={{ fontSize: 10, color: '#848484' }}>Order ID - {props.order.orderId}</Typography>
+					<Typography variant='subtitle1' style={{ color: '#ffffff' }}>{props.order.side} {props.order.ticker}</Typography>
+					{props.order.status === 'filled'
+						? <Typography variant='body1' style={{ color: '#e0e0e0' }}>{props.order.filledQuantity} @ ${(props.order.filledAmount / props.order.filledQuantity).toFixed(2)}</Typography>
+						: <Typography variant='body1' style={{ color: '#e0e0e0' }}>{props.order.orderedAmount} @ Market </Typography>
+					}
+				</Grid> 
+			</Grid>
+		</div>
+		)
+}
--- Web/Web/ClientApp/src/components/Trading/OrderHistory.js
+++ Web/Web/ClientApp/src/components/Trading/OrderHistory.js
@@ -1,14 +1,9 @@
-﻿import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Typography from '@material-ui/core/Typography';
-import React, {useEffect, useState } from 'react';
+﻿import React, { useEffect, useState } from 'react';
 import AuthClient from '../../Clients/AuthClient';
 import OrderClient from '../../Clients/OrderClient';
 import UserClient from '../../Clients/UserClient';
-import PrimaryTextButton from '../Generic/PrimaryTextButton';
+import OrderCard from './OrderCard';
+import Grid from '@material-ui/core/Grid';
 
 export default function OrderHistory(props) {
 	const [orders, setOrders] = useState()
@@ -30,66 +25,12 @@ export default function OrderHistory(props) {
 	useEffect(() => { loadOrderHistory() }, [])
 
 	return (
-		<>
-			<Typography variant="h6" gutterBottom component="div">
-				Orders
-			</Typography>
-			<Table size="small" aria-label="purchases">
-				<TableHead>
-					<TableRow>
-						<TableCell style={{ color: '#d0d0d0' }}>Order Id</TableCell>
-						<TableCell style={{ color: '#d0d0d0' }}>Ticker</TableCell>
-						<TableCell style={{ color: '#d0d0d0' }}>Created Time</TableCell>
-						<TableCell style={{ color: '#d0d0d0' }}>Filled Time</TableCell>
-						<TableCell style={{ color: '#d0d0d0' }}>Direction</TableCell>
-						<TableCell style={{ color: '#d0d0d0' }}>Order Amount</TableCell>
-						<TableCell style={{ color: '#d0d0d0' }}>Order Quantity</TableCell>
-						<TableCell style={{ color: '#d0d0d0' }}>Filled Amount</TableCell>
-						<TableCell style={{ color: '#d0d0d0' }}>Filled Quantity</TableCell>
-						<TableCell style={{ color: '#d0d0d0' }}>Status</TableCell>
-						<TableCell style={{ color: '#d0d0d0' }}></TableCell>
-					</TableRow>
-				</TableHead>
-				<TableBody>
-					{orders && orders.slice(0).reverse().map((order) => (
-						<TableRow key={orders.OrderId}>
-							<TableCell style={{ color: '#d0d0d0' }}>
-								{order.orderId}
-							</TableCell>
-							<TableCell style={{ color: '#d0d0d0' }}>
-								{order.ticker}
-							</TableCell>
-							<TableCell style={{ color: '#d0d0d0' }}>
-								{order.createdAt}
-							</TableCell>
-							<TableCell style={{ color: '#d0d0d0' }}>
-								{order.filledAt ? order.filledAt : "--"}
-							</TableCell>
-							<TableCell style={{ color: '#d0d0d0' }}>
-								{order.side}
-							</TableCell>
-							<TableCell style={{ color: '#d0d0d0' }}>
-								{order.orderedAmount ? order.orderedAmount : "--"}
-							</TableCell>
-							<TableCell style={{ color: '#d0d0d0' }}>
-								{order.orderedQuantity ? order.orderedQuantity : "--"}
-							</TableCell>
-							<TableCell style={{ color: '#d0d0d0' }}>
-								{order.filledAmount ? order.filledAmount : "--"}
-							</TableCell>
-							<TableCell style={{ color: '#d0d0d0' }}>
-								{order.filledQuantity ? order.filledQuantity : "--"}
-							</TableCell>
-							<TableCell style={{ color: '#d0d0d0' }}>
-								{order.status}
-							</TableCell>
-							<TableCell style={{ color: '#d0d0d0' }}>
-								{(order.status !== "filled" && order.status !== "canceled") ? <PrimaryTextButton text="Cancel Order" onClick={() => cancelOrder(order)} /> : <></>}
-							</TableCell>
-						</TableRow>
-					))}
-				</TableBody>
-			</Table>
-		</>
+		<Grid container spacing={1}>
+			{orders && orders.slice(0).reverse().map((order) => (
+				<Grid item xs={12}>
+					<OrderCard order={order} />
+				</Grid>
+			))}
+		</Grid>
 	)
 }
